19327ce4e109e26b9072e9b188ee78f5e3ee1d6f
BrahianCarrera <brahian.carrera@udea.edu.co>
2024-04-18 21:20:33 -0500

    Created the password Manager class and the basic structure of the project?

--- PasswordManager/PasswordManager/Form1.cs
+++ PasswordManager/PasswordManager/Form1.cs
@@ -16,5 +16,15 @@ namespace PasswordManager
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
